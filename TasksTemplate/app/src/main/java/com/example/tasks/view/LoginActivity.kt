6c0950dca4f8bfadeaa533550a869fa1b734e136
geraldo.pires <geraldopireslacerda771@gmail.com>
2021-09-01 12:48:34 -0300

    Implementação Login - Parte 2

--- TasksTemplate/app/src/main/java/com/example/tasks/view/LoginActivity.kt
+++ TasksTemplate/app/src/main/java/com/example/tasks/view/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.tasks.R
 import com.example.tasks.viewmodel.LoginViewModel
@@ -55,6 +57,15 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
      */
     private fun observe() {
 
+        mViewModel.login.observe(this, Observer {
+            if(it){
+                startActivity(Intent(this, MainActivity::class.java))
+
+            }else
+            {
+                Toast.makeText(this, "Erro ao logar", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     /**
