1837f1614e0dd318627535ee188f862d68baf347
geraldo <geraldopireslacerda771@gmail.com>
2021-09-02 23:16:47 -0300

    Serviçoes e modelos

---
+++ TasksTemplate/app/src/main/java/com/example/tasks/service/models/PriorityModel.kt
@@ -0,0 +1,12 @@
+package com.example.tasks.service.models
+
+import com.google.gson.annotations.SerializedName
+
+class PriorityModel {
+
+    @SerializedName("Id")
+    var id: Int = 0
+
+    @SerializedName("Description")
+    var description: String = ""
+}
